87916140df90e24a1f81b095b618a134d9423235
hugecat <hugecat@Chieh-MBPR.local>
2016-11-06 23:00:21 +0800

    add phone version files

---
+++ js/custom.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+  $("#but1").hover(function () {
+      $("#but1").prop("src", "images/uploads/buttons/button1_change.png");
+  }, function () {
+      $("#but1").prop("src", "images/uploads/buttons/button1.png");
+  });
+
+  $("#but2").hover(function () {
+      $("#but2").prop("src", "images/uploads/buttons/button2_change.png");
+  }, function () {
+      $("#but2").prop("src", "images/uploads/buttons/button2.png");
+  });
+
+  $("#1-1").hover(function () {
+      $("#1-1").prop("src", "images/uploads/topic1/1-1d.png");
+  }, function () {
+      $("#1-1").prop("src", "images/uploads/topic1/1-1.png");
+  });
+
+  $("#1-2").hover(function () {
+      $("#1-2").prop("src", "images/uploads/topic1/1-2d.png");
+  }, function () {
+      $("#1-2").prop("src", "images/uploads/topic1/1-2.png");
+  });
+
+  $("#1-3").hover(function () {
+      $("#1-3").prop("src", "images/uploads/topic1/1-3d.png");
+  }, function () {
+      $("#1-3").prop("src", "images/uploads/topic1/1-3.png");
+  });
+
+  $("#2-1").hover(function () {
+      $("#2-1").prop("src", "images/uploads/topic2/2-1d.png");
+  }, function () {
+      $("#2-1").prop("src", "images/uploads/topic2/2-1.png");
+  });
+
+  $("#2-2").hover(function () {
+      $("#2-2").prop("src", "images/uploads/topic2/2-2d.png");
+  }, function () {
+      $("#2-2").prop("src", "images/uploads/topic2/2-2.png");
+  });
+
+  $("#2-3").hover(function () {
+      $("#2-3").prop("src", "images/uploads/topic2/2-3d.png");
+  }, function () {
+      $("#2-3").prop("src", "images/uploads/topic2/2-3.png");
+  });
+
+  $("#2-4").hover(function () {
+      $("#2-4").prop("src", "images/uploads/topic2/2-4d.png");
+  }, function () {
+      $("#2-4").prop("src", "images/uploads/topic2/2-4.png");
+  });
+
+  $("#3-1").hover(function () {
+      $("#3-1").prop("src", "images/uploads/topic3/3-1d.png");
+  }, function () {
+      $("#3-1").prop("src", "images/uploads/topic3/3-1.png");
+  });
+
+  $("#3-2").hover(function () {
+      $("#3-2").prop("src", "images/uploads/topic3/3-2d.png");
+  }, function () {
+      $("#3-2").prop("src", "images/uploads/topic3/3-2.png");
+  });
+
+  $("#3-3").hover(function () {
+      $("#3-3").prop("src", "images/uploads/topic3/3-3d.png");
+  }, function () {
+      $("#3-3").prop("src", "images/uploads/topic3/3-3.png");
+  });
+
+  $("#3-4").hover(function () {
+      $("#3-4").prop("src", "images/uploads/topic3/3-4d.png");
+  }, function () {
+      $("#3-4").prop("src", "images/uploads/topic3/3-4.png");
+  });
+
+  $("#3-5").hover(function () {
+      $("#3-5").prop("src", "images/uploads/topic3/3-5d.png");
+  }, function () {
+      $("#3-5").prop("src", "images/uploads/topic3/3-5.png");
+  });
+
+
+  $("#sec1").hover(function () {
+      $("#sec1").prop("src", "images/uploads/phone/one_open.png");
+  }, function () {
+      $("#sec1").prop("src", "images/uploads/phone/one.png");
+  });
+
+  $("#sec2").hover(function () {
+      $("#sec2").prop("src", "images/uploads/phone/two_open.png");
+  }, function () {
+      $("#sec2").prop("src", "images/uploads/phone/two.png");
+  });
+
+  $("#sec3").hover(function () {
+      $("#sec3").prop("src", "images/uploads/phone/three_open.png");
+  }, function () {
+      $("#sec3").prop("src", "images/uploads/phone/three.png");
+  });
+
+});
